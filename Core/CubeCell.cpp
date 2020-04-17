22b06227d2b2c731fa9592c29e5614d168f08dcc
TheTytan303 <50801423+TheTytan303@users.noreply.github.com>
2020-04-17 20:53:41 +0200

    Update 17.04.2020

--- Core/CubeCell.cpp
+++ Core/CubeCell.cpp
@@ -5,7 +5,6 @@ std::vector<float> CubeCell::getColor()
 	return color;
 }
 
-
 CubeCell::CubeCell(unsigned short* meshSize, unsigned short x, unsigned short y, unsigned short z, std::vector<float> color, std::vector<float> values)
 	:
 	values(values),
@@ -21,28 +20,27 @@ CubeCell::CubeCell(unsigned short* meshSize, unsigned short x, unsigned short y,
 	//float tmpZ = 2* (float)z / ((float)meshSize[2]) -1;
 
 	float s = 2.0f/(float)meshSize[0];
-	//s *= 0.9f;
+	s *= 0.9f;
 	//float s = 0.6f;
 	float tmpX = (float)(x*2)/ (float)meshSize[0] -1.0f +(s / 2);
 	float tmpY = (float)(y * 2) / (float)meshSize[1] - 1.0f + (s / 2);
 	float tmpZ = (float)(z * 2) / (float)meshSize[2] - 1.0f + (s / 2);
 
 
-	this->vertices[0] = {tmpX - s, tmpY - s, tmpZ - s },	//LBB - 0
-	this->vertices[1] =	{tmpX,   tmpY-s, tmpZ-s};	//RBB - 1
-	this->vertices[2] = {tmpX-s, tmpY,   tmpZ-s};	//LBT - 2
-	this->vertices[3] = {tmpX,   tmpY,   tmpZ-s};	//RBT - 3
-	this->vertices[4] = {tmpX-s, tmpY-s, tmpZ};	//LFB - 4
-	this->vertices[5] = {tmpX,   tmpY-s, tmpZ};	//RFB - 5
-	this->vertices[6] = {tmpX-s, tmpY,   tmpZ};	//LFT - 6
-	this->vertices[7] = {tmpX,   tmpY,   tmpZ};	//RFT - 7
+	this->vertices[0] = {tmpX - s, tmpY - s, tmpZ - s	, color[0], color[1], color[2]},//LBB - 0
+	this->vertices[1] =	{tmpX,   tmpY-s, tmpZ-s			, color[0], color[1], color[2]};	//RBB - 1
+	this->vertices[2] = {tmpX-s, tmpY,   tmpZ-s			, color[0], color[1], color[2]};	//LBT - 2
+	this->vertices[3] = {tmpX,   tmpY,   tmpZ-s			, color[0], color[1], color[2]};	//RBT - 3
+	this->vertices[4] = {tmpX-s, tmpY-s, tmpZ			, color[0], color[1], color[2]};	//LFB - 4
+	this->vertices[5] = {tmpX,   tmpY-s, tmpZ			, color[0], color[1], color[2]};	//RFB - 5
+	this->vertices[6] = {tmpX-s, tmpY,   tmpZ			, color[0], color[1], color[2]};	//LFT - 6
+	this->vertices[7] = {tmpX,   tmpY,   tmpZ			, color[0], color[1], color[2]};	//RFT - 7
 }
 
 Cell::Vertex* CubeCell::getVertices()
 {
 	return vertices;
 }
-
 
 unsigned short* CubeCell::getCoords()
 {
@@ -54,7 +52,7 @@ int CubeCell::getId()
 	return id;
 }
 
-double* CubeCell::getDetails()
+std::vector<float> CubeCell::getDetails()
 {
-	return nullptr;
+	return values;
 }
