9ae43c247fe8d378b01ced18b14d896d750d6c15
TheTytan303 <50801423+TheTytan303@users.noreply.github.com>
2020-04-17 20:54:12 +0200

    Update 17.04.2020

--- Core/CubeCell.h
+++ Core/CubeCell.h
@@ -1,6 +1,26 @@
 #pragma once
 #include "Cell.h"
 #include <vector>
+#include <string>
+
+/*
+	Vertex verticies - komórka sześcienna przechowuje położenie swoich 8 wierzchołków - obliczone na podstawie rozmiaru całkowitego siatki
+	
+	color - kazda sześcienna komórka pamięta swój kolor do narysowania (w postaci 3x float: 0.0f - 1.0f) - aktualnie
+	// mozna zmienić na 3 unsigned shorty (mniejszy nakład danych?) 
+	// albo przechowywać id ziarna do którego komórka należy, a w "ziarnie" niech znajdują się 3x float i pewnie jego własciwości
+	// albo kolory przechowywać w DataMiner
+
+	valuesNames - właściwości komórki specyfikowane w danych wejściowych 
+	//można przechowywać jedną talicę nazw, a nie w każdej komórce (static? - zakładałby, że każda komórka będzie miała takie same rodzaje właściwości)
+	//albo przechowywać w DataMiner
+
+	values - właściwości komórki specyfikowane w danych wejściowych
+
+	konstruktor - rozmiar siatki potrzebny do obliczenia bezwzględnych pozycji wierzchołków komórki
+*/
+
+
 class CubeCell : public Cell
 {
 private:
@@ -10,6 +30,7 @@ public:
 	int id;
 	Vertex vertices[8];
 	std::vector<float> color;
+	//std::vector<std::string> valuesNames;
 	std::vector<float> values;
 public:
 	CubeCell(unsigned short* meshSize, unsigned short x, unsigned short y, unsigned short z, std::vector<float> color, std::vector<float> values);
@@ -23,6 +44,6 @@ public:
 
 	virtual int getId() override;
 
-	virtual double* getDetails() override;
+	virtual std::vector<float> getDetails() override;
 
 };
