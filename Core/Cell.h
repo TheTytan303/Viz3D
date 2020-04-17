832d3a9df8b09d9dc5b82b1dff240b48fd6b2250
TheTytan303 <50801423+TheTytan303@users.noreply.github.com>
2020-04-17 20:53:10 +0200

    Update 17.04.2020

--- Core/Cell.h
+++ Core/Cell.h
@@ -1,5 +1,12 @@
 #pragma once
-
+#include <vector>
+/*
+	Reprezentacja komórki :
+	struktura "vertex" reprezentuje wierzchołek, potrzebne do rysowania w przestrzeni przez DirectX 
+	// oraz przechowuje kolory w tych wierzchołkach
+	getCoords - zwraca współrzędne tej komórki w całlej strukturze, zwróci tablicę 3x unsigned short
+	getDetails - zwraca tablicę float'ów, które reprezentują parametry komórki
+*/
 class Cell
 {
 public:
@@ -9,11 +16,16 @@ public:
 			float y;
 			float z;
 		} pos;
+		struct {
+			float r;
+			float g;
+			float b;
+		}color;
 	};
 public:
 	//
 	virtual unsigned short* getCoords() = 0;
 	virtual int getId() = 0;
-	virtual double* getDetails() = 0;
+	virtual  std::vector<float> getDetails() = 0;
 
 };
